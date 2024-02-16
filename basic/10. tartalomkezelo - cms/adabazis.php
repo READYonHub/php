1cfbec26716a3e7a29a4ea15b7d994b3b194b588
READYonHub <104925582+READYonHub@users.noreply.github.com>
2024-02-16 15:41:18 +0100

    Add files via upload

---
+++ basic/10. tartalomkezelo - cms/adabazis.php
@@ -0,0 +1,9 @@
+
+<?php
+
+//ini_set("display_errors", 1);
+//error_reporting(E_ALL);
+
+$dbconn = mysqli_connect("localhost", "root", "", "tartalomkezelo");
+mysqli_query($dbconn, "SET NAMES utf8");
+?>
--- basic/10. tartalomkezelo - cms/index.php
+++ basic/10. tartalomkezelo - cms/index.php
@@ -1,35 +1,59 @@
 <?php
+require_once('adabazis.php');
 
-require_once("adatbazis.php");
-$sql = "";
+/*-------------------- Menu osszeallitasa */
+$sql = "SELECT id, allias, menunev
+        FROM cms_tartalom
+        WHERE statusz = 1
+        ORDER BY sorrend ASC";
 
-$leiras = "Az oldal pár mondatos leírása...";
-$kulcsszavak = "kulcsszó1, kulcsszó2";
-$menu = "
-            <ul>
-                <li><a href=\"#\" class=\"activ\">Bemutatkozás</a></li>
-                <li><a href=\"#\">Kedvencek</a></li>
-                <li><a href=\"#\">Képgaléria</a></li>
-                <li><a href=\"#\">Kapcsolat</a></li>
-            </ul>
-";
+$eredmeny = mysqli_query($dbconn, $sql);
 
-$menunev = "Bemutatkozás";
-$tartalom = "<h2>Hello világ</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sollicitudin arcu vitae ex tempor malesuada. Proin consectetur elementum est, non vehicula est hendrerit aliquet. Donec accumsan rutrum consectetur. Nulla tincidunt nisl venenatis augue venenatis, sit amet cursus arcu congue. Donec suscipit vehicula neque. Cras malesuada aliquam lorem, quis semper ex blandit ut. Curabitur ornare eleifend condimentum. Donec molestie ex orci, sed malesuada lectus sagittis et. Ut nec elementum eros. Aliquam vel imperdiet orci. Nunc viverra, nisi nec laoreet lacinia, est mauris ultrices ante, vel posuere nisi magna a elit. Suspendisse gravida arcu eget mauris pulvinar cursus. Ut sed dapibus ligula. Aliquam condimentum vel augue vulputate aliquam. Curabitur at sapien at odio sagittis blandit sed sit amet sapien. In blandit bibendum libero, nec ullamcorper libero imperdiet ut.</p>
-                <p>Proin placerat urna id sapien volutpat, sed cursus massa euismod. Suspendisse et nisi commodo, efficitur ante sed, semper felis. Integer eu vulputate nibh, eu malesuada nunc. Integer fermentum nec quam at elementum. Quisque posuere sem non tellus mollis tristique. Etiam nec urna aliquet, porta lectus vehicula, hendrerit elit. Maecenas non felis ac eros luctus placerat. Ut scelerisque augue et finibus porttitor. Vestibulum non molestie tortor.</p>
-                <p>Quisque eget vulputate urna. Vestibulum pretium venenatis urna, nec malesuada velit lacinia a. Donec rutrum enim id enim ornare vulputate. Praesent ornare, dui sit amet sodales tincidunt, erat sem pellentesque augue, ac vestibulum neque risus consequat erat. Suspendisse potenti. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam ornare massa sit amet turpis commodo venenatis sed a neque. Vestibulum vitae pretium est, sed sollicitudin ante. Nunc id felis dui. Nam dolor lorem, molestie vitae enim ac, condimentum porta sapien. Curabitur mollis venenatis semper. Vestibulum vestibulum pellentesque lorem vel semper. Mauris purus orci, maximus vitae felis eu, accumsan commodo metus. Nam odio magna, venenatis eget hendrerit tincidunt, aliquam in est. Cras dictum, leo ac lobortis lacinia, neque diam feugiat ex, eget dictum nibh erat a tortor. Maecenas quis ex mi.</p>
-";
+$menu = "<ul>\n";
+while ($sor = mysqli_fetch_assoc($eredmeny)) {
+        $menu  .= "<li><a href=\"index.php?id={$sor['id']}\">{$sor['menunev']}</a></li>\n";
+}
+$menu .= "</ul>\n";
 
+/*-------------------- tartalom elkeszitese */
+$id = (isset($_GET['id'])) ? $_GET['id'] : 1;
+$sql = "SELECT menunev, tartalom, letrehozas, modositas, leiras, kulcsszavak
+        FROM cms_tartalom 
+        WHERE id = {$id}
+        LIMIT 1 ";
+
+$eredmeny = mysqli_query($dbconn, $sql);
+
+/* Érvényes tartalom */
+if (mysqli_num_rows($eredmeny) == 1) {
+        $sor = mysqli_fetch_assoc($eredmeny);
+        $leiras         = $sor['leiras'];
+        $kulcsszavak    = $sor['kulcsszavak'];
+        $menunev        = $sor['menunev'];
+        $tartalom       = $sor['tartalom'];
+        $letrehozas     = $sor['letrehozas'];
+}
+/* Az oldal nem található */ else {
+        $leiras                 = "";
+        $kulcsszavak            = "";
+        $menunev                = "Hiba";
+        $tartalom               = "<p><em>A keresett oldal nem találtahtó...</em></p>";
+        $letrehozas             =  date("Y-m-d H:i:s");
+}
+
+/* Modulok kezelése */
 $oldalsav = "";
 
 $sablon = file_get_contents("sablon.html");
-$sablon = str_replace("{{menu}}",           $menu, $sablon);
-$sablon = str_replace("{{menunev}}",        $menunev, $sablon);
-$sablon = str_replace("{{tartalom}}",       $tartalom, $sablon);
-$sablon = str_replace("{{oldalsav}}",       $oldalsav, $sablon);
-$sablon = str_replace("{{leiras}}",         $leiras, $sablon);
-$sablon = str_replace("{{kulcsszavak}}",    $kulcsszavak, $sablon);
-
+$sablon = str_replace("{{menu}}",           $menu,              $sablon);
+$sablon = str_replace("{{menunev}}",        $menunev,           $sablon);
+$sablon = str_replace("{{tartalom}}",       $tartalom,          $sablon);
+$sablon = str_replace("{{letrehozas}}",     $letrehozas,          $sablon);
+$sablon = str_replace("{{oldalsav}}",       $oldalsav,          $sablon);
+$sablon = str_replace("{{leiras}}",         $leiras,            $sablon);
+$sablon = str_replace("{{kulcsszavak}}",    $kulcsszavak,       $sablon);
 
 print $sablon;
+
+print_r($dbconn);
+mysqli_close($dbconn);
